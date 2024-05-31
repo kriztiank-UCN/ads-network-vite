7c415ed45b8301e14dd02524f07dc7ab33b80bb8
Kristian <10221425@ucn.dk>
2024-05-31 10:29:35 +0200

    Display notification for new message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ import { AuthContext } from "../context/auth"
 import { auth, db } from "../firebaseConfig"
 
 const Navbar = () => {
-  // destructure the user object from the AuthContext
-  const { user } = useContext(AuthContext)
+  // destructure the user & unread from the AuthContext
+  const { user, unread } = useContext(AuthContext)
   const navigate = useNavigate()
 
   const handleSignout = async () => {
@@ -27,6 +27,7 @@ const Navbar = () => {
         <Link className="navbar-brand" to="/">
           Ads Network
         </Link>
+         {/* hamburger menu */}
         <button
           className="navbar-toggler"
           type="button"
@@ -44,12 +45,19 @@ const Navbar = () => {
             {user ? (
               <>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/chat">
+                {/* if length is greater than zero, there are unread messages */}
+                  <Link className="nav-link position-relative" to="/chat">
                     Chat
+                    {unread.length ? (
+                      // if there are unread messages, display a red circle with a bootstrap badge
+                      <span className="position-absolute top-10 start-90 translate-middle p-1 bg-danger border border-light rounded-circle">
+                        <span className="visually-hidden">New alerts</span>
+                      </span>
+                    ) : null}
                   </Link>
                 </li>
                 <li className="nav-item">
-                  {/* get the user object from the AuthContext */}
+                  {/* get the user from the AuthContext */}
                   <Link className="nav-link" to={`/profile/${user.uid}`}>
                     Profile
                   </Link>
