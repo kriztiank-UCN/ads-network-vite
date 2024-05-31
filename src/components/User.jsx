5a2ad7358941285867ad8eb3b53d5d344714b5c4
Kristian <10221425@ucn.dk>
2024-05-31 07:37:14 +0200

    Display conversation with selected user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/prop-types */
 import { FaUserCircle } from "react-icons/fa"
 
-const User = ({ user }) => {
+const User = ({ user, selectUser, chat }) => {
   return user ? (
-    <div className="d-flex align-items-center justify-content-center justify-content-md-start my-2 p-1">
+    <div
+      className={`d-flex align-items-center justify-content-center justify-content-md-start my-2 p-1 ${
+        user.ad.title === chat?.ad.title ? "gray" : ""
+      }`}
+      onClick={() => selectUser(user)}
+      style={{ cursor: "pointer" }}
+    >
       {user.photoUrl ? (
         <img
           src={user.photoUrl}
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -4,101 +4,130 @@ import {
   doc,
   getDoc,
   getDocs,
+  onSnapshot,
+  orderBy,
   query,
   Timestamp,
   where,
-} from "firebase/firestore"
-import { useEffect, useState } from "react"
-import { db, auth } from "../firebaseConfig"
-import { useLocation, Link } from "react-router-dom"
-import MessageForm from "../components/MessageForm"
-import User from "../components/User"
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db, auth } from "../firebaseConfig";
+import { useLocation, Link } from "react-router-dom";
+import MessageForm from "../components/MessageForm";
+import User from "../components/User";
+import Message from "../components/Message";
 
 const Chat = () => {
-  const [chat, setChat] = useState()
-  const [text, setText] = useState("")
-  const [users, setUsers] = useState([])
+  const [chat, setChat] = useState();
+  const [text, setText] = useState("");
+  const [users, setUsers] = useState([]);
+  const [msgs, setMsgs] = useState([]);
 
-  const location = useLocation()
+  const location = useLocation();
 
-  const user1 = auth.currentUser.uid
+  const user1 = auth.currentUser.uid;
 
-  const getChat = async ad => {
-    const buyer = await getDoc(doc(db, "users", user1))
-    const seller = await getDoc(doc(db, "users", ad.postedBy))
-    setChat({ ad, me: buyer.data(), other: seller.data() })
-  }
+  const selectUser = async (user) => {
+    setChat(user);
+
+    const user2 = user.other.uid;
+    const id =
+      user1 > user2
+        ? `${user1}.${user2}.${user.ad.adId}`
+        : `${user2}.${user1}.${user.ad.adId}`;
+
+    const msgsRef = collection(db, "messages", id, "chat");
+    const q = query(msgsRef, orderBy("createdAt", "asc"));
+
+    onSnapshot(q, (querySnapshot) => {
+      let msgs = [];
+      querySnapshot.forEach((doc) => msgs.push(doc.data()));
+      setMsgs(msgs);
+    });
+  };
+
+  const getChat = async (ad) => {
+    const buyer = await getDoc(doc(db, "users", user1));
+    const seller = await getDoc(doc(db, "users", ad.postedBy));
+    setChat({ ad, me: buyer.data(), other: seller.data() });
+  };
 
   const getList = async () => {
-    const msgRef = collection(db, "messages")
-    const q = query(msgRef, where("users", "array-contains", user1))
+    const msgRef = collection(db, "messages");
+    const q = query(msgRef, where("users", "array-contains", user1));
 
-    const msgsSnap = await getDocs(q)
-    const messages = msgsSnap.docs.map(doc => doc.data())
+    const msgsSnap = await getDocs(q);
+    const messages = msgsSnap.docs.map((doc) => doc.data());
 
-    const users = []
+    const users = [];
     for (const message of messages) {
-      const adRef = doc(db, "ads", message.ad)
+      const adRef = doc(db, "ads", message.ad);
       const meRef = doc(
         db,
         "users",
-        message.users.find(id => id === user1)
-      )
+        message.users.find((id) => id === user1)
+      );
       const otherRef = doc(
         db,
         "users",
-        message.users.find(id => id !== user1)
-      )
+        message.users.find((id) => id !== user1)
+      );
 
-      const adDoc = await getDoc(adRef)
-      const meDoc = await getDoc(meRef)
-      const otherDoc = await getDoc(otherRef)
+      const adDoc = await getDoc(adRef);
+      const meDoc = await getDoc(meRef);
+      const otherDoc = await getDoc(otherRef);
 
       users.push({
         ad: adDoc.data(),
         me: meDoc.data(),
         other: otherDoc.data(),
-      })
+      });
     }
-    setUsers(users)
-  }
+    setUsers(users);
+  };
 
   useEffect(() => {
     if (location.state?.ad) {
-      getChat(location.state?.ad)
+      getChat(location.state?.ad);
     }
-    getList()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    getList();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  // console.log(users);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const handleSubmit = async e => {
-    e.preventDefault()
-
-    const user2 = chat.other.uid
+    const user2 = chat.other.uid;
     const chatId =
-      user1 > user2 ? `${user1}.${user2}.${chat.ad.adId}` : `${user2}.${user1}.${chat.ad.adId}`
+      user1 > user2
+        ? `${user1}.${user2}.${chat.ad.adId}`
+        : `${user2}.${user1}.${chat.ad.adId}`;
 
     await addDoc(collection(db, "messages", chatId, "chat"), {
       text,
       sender: user1,
       createdAt: Timestamp.fromDate(new Date()),
-    })
-    setText("")
-  }
+    });
+    setText("");
+  };
 
   return (
     <div className="row g-0">
-      <div className="col-2 col-md-4 users_container" style={{ borderRight: "1px solid #ddd" }}>
+      <div
+        className="col-2 col-md-4 users_container"
+        style={{ borderRight: "1px solid #ddd" }}
+      >
         {users.map((user, i) => (
-          <User key={i} user={user} />
+          <User key={i} user={user} selectUser={selectUser} chat={chat} />
         ))}
       </div>
       <div className="col-10 col-md-8 position-relative">
         {chat ? (
           <>
-            <div className="text-center mt-1" style={{ borderBottom: "1px solid #ddd" }}>
+            <div
+              className="text-center mt-1"
+              style={{ borderBottom: "1px solid #ddd" }}
+            >
               <h3>{chat.other.name}</h3>
             </div>
             <div className="p-2" style={{ borderBottom: "1px solid #ddd" }}>
@@ -122,7 +151,16 @@ const Chat = () => {
                 </div>
               </div>
             </div>
-            <MessageForm text={text} setText={setText} handleSubmit={handleSubmit} />
+            <div className="messages overflow-auto">
+              {msgs.map((msg, i) => (
+                <Message key={i} msg={msg} user1={user1} />
+              ))}
+            </div>
+            <MessageForm
+              text={text}
+              setText={setText}
+              handleSubmit={handleSubmit}
+            />
           </>
         ) : (
           <div className="text-center mt-5">
@@ -131,7 +169,7 @@ const Chat = () => {
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
